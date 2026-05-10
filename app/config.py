a34c3a9bcbddb5d18543eaa76144e34ae2af1798
Javier <javi.ies9023@gmail.com>
2026-05-10 20:25:17 -0300

    modifico config y requirements, agrego .env

--- app/config.py
+++ app/config.py
@@ -1,10 +1,13 @@
 #importa modulo os que accede a variables de entorno y 
 # maneja las rutas del sistema
 import os
+from dotenv import load_dotenv
+ 
+load_dotenv()
 
 class Config:
-    SECRET_KEY = "super_secret_key"#Eso evita hardcodear claves sensibles.
+    SECRET_KEY = os.getenv("SECRET_KEY")
     #esto indica que base de datos usar 
-    SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:@localhost/db_flask"
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
     #Esto desactiva un sistema interno de seguimiento de cambios.
     SQLALCHEMY_TRACK_MODIFICATIONS = False
